f3fc5e2b7ea07b6f71f404c39b60c7c9e0c23fe6
asanati <asanati@student.ethz.ch>
2022-03-11 13:59:13 +0100

    Added 3D grid

--- src/graph.h
+++ src/graph.h
@@ -9,10 +9,14 @@
 class Graph {
     public:
         Graph(const std::vector<std::vector<int>>& grid);
+        Graph(const std::vector<std::vector<std::vector<int>>>& grid);
         void setEdges(const std::vector<std::vector<int>>& grid, int i, int j);
+        void setEdges(const std::vector<std::vector<std::vector<int>>>& grid, int i, int j, int k);
         void printAdjMatrix();
         int convertTo1D(int i, int j);
+        int convertTo1D(int i, int j, int k);
         std::vector<int> convertTo2D(int vertex);
+        std::vector<int> convertTo3D(int vertex);
         std::vector<int> getAdjacent(int u);
         int minDistanceVertex(std::vector<int> distances, std::vector<bool> sptSet);
         int minDistanceAstar(std::vector<int> f, std::vector<int> open);
@@ -22,6 +26,7 @@ class Graph {
         std::vector<int> astar(int u, int v);
         bool calculateAstarPath(int u, int v, std::vector<int>* parents);
     private:
-        int numVertices, numRows, numCols;
+        int numVertices, numRows, numCols, numx, numy, numz;
         std::vector<std::vector<int>> adjMatrix;
+        std::vector<std::vector<std::vector<int>>> adjMatrix3D;
 };
